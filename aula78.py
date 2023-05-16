9d34271607126d6887a57c876d4ec548918bcd4e
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2023-05-16 12:13:54 -0300

    aula78.py

--- aula78.py
+++ aula78.py
@@ -10,3 +10,24 @@
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
 
+pessoa = {
+    'nome':'Rafa',
+    'sobrenome':'Lima',
+    'idade':'17'
+    #'altura': 2
+}
+
+pessoa.setdefault('altura', None) #se não tiver a chave ele usa um valor padrão
+print(pessoa['altura'])
+print(len(pessoa)) # retorna o número de chaves
+print(pessoa.keys()) # retorna as chaves
+print(list(pessoa.keys())) # podem ser covertidos pra listas e tuples
+print(pessoa.values()) # retorna só os valores
+print(list(pessoa.items())) # retorna chave e valor
+for chave, valor in pessoa.items():
+    print(chave, valor)
+
+
+
+
+
