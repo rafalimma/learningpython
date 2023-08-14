bcdf0a492ee6c16e18aca7fcdd8d6a4d93e52a4e
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2023-08-14 18:45:20 -0300

    Add files via upload

---
+++ aula118.py
@@ -0,0 +1,33 @@
+# reduce - faz a redução de um iterável em um valor
+from functools import reduce
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10},
+    {'nome': 'Produto 1', 'preco': 22},
+    {'nome': 'Produto 3', 'preco': 2},
+    {'nome': 'Produto 2', 'preco': 6},
+    {'nome': 'Produto 4', 'preco': 4},
+]
+def funcao_do_reduce(acumulador, produto):
+    print('acumulador', acumulador)
+    print('produto', produto)
+    print()
+    return acumulador + produto['preco']
+
+total = reduce( #sempre passa acumulador e produto que esta iterando
+    funcao_do_reduce,
+    produtos,
+    0 #valor inicial do total (acumulador)
+    #delimita o tipo também
+)
+
+print("o total é", total)
+
+
+'''
+total = 0
+for p in produtos:
+    total += p['preco']
+
+print(total)
+'''
