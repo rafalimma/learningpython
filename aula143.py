774ee248483db3de72664968b9bc7625f1ad7c1f
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2023-10-15 22:12:40 -0300

    Add files via upload

---
+++ aula143.py
@@ -0,0 +1,13 @@
+#hereança
+# Herança simples - Relações entre classes
+# Associação - usa, Agregação - tem
+# Composição - É dono de, Herança - É um
+#
+# Herança vs Composição
+#
+# Classe principal (Pessoa)
+#   -> super class, base class, parent class
+# Classes filhas (Cliente)
+#   -> sub class, child class, derived class
+
+#quando vc cria uma classe vc cria um tipo pra o programa
