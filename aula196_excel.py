8e28f5d842dc09a617a8433b18125add5f6475e1
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2025-07-09 16:48:23 -0700

    Add files via upload

---
+++ aula196_excel.py
@@ -0,0 +1,44 @@
+# openpyxl para arquivos Excel xlsx, xlsm, xltx e xltm (instalação)
+# Com essa biblioteca será possível ler e escrever dados em células
+# específicas, formatar células, inserir gráficos,
+# criar fórmulas, adicionar imagens e outros elementos gráficos às suas
+# planilhas. Ela é útil para automatizar tarefas envolvendo planilhas do
+# Excel, como a criação de relatórios e análise de dados e/ou facilitando a
+# manipulação de grandes quantidades de informações.
+# Instalação necessária: pip install openpyxl
+# Documentação: https://openpyxl.readthedocs.io/en/stable/
+
+from pathlib import Path
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+ROOT_FOLDER = Path(__file__).parent
+WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
+
+workbook = openpyxl.Workbook()
+worksheet: Worksheet = workbook.active
+worksheet.cell(1, 1, 'Nome')
+worksheet.cell(1, 2, 'Idade')
+worksheet.cell(1, 3, 'Nota')
+
+students = [
+    # nome      idade nota
+    ['João',    14,   5.5],
+    ['Maria',   13,   9.7],
+    ['Luiz',    15,   8.8],
+    ['Alberto', 16,   10],
+]
+
+# for i in range(2, 10):
+#     for j in range(1, 4):
+#         print("Linha", i, "Coluna", j)
+
+# for i, student_row in enumerate(students, start=2):
+#     for j, student_column in enumerate(student_row, start=1):
+#         print(i, j, student_column)
+
+for student in students:
+    worksheet.append(student)
+
+workbook.save(WORKBOOK_PATH)
