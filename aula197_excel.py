8e8e5db73087e9fbf90b94379114d9bad638a219
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2025-07-11 15:47:24 -0300

    Update aula197_excel.py

--- aula197_excel.py
+++ aula197_excel.py
@@ -1,13 +1,24 @@
 from pathlib import Path
 import openpyxl
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
 
 ROOT_FOLDER = Path(__file__).parent
 WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
 
-workbook = Workbook()
+workbook: Workbook = load_workbook(WORKBOOK_PATH)
 
-workbook.create_sheet('RAFA', 0) # mudar ordem do nome da planilha
-worksheet: Worksheet = workbook['RAFA']
+worksheet: Worksheet = workbook.active
+
+for row in worksheet.iter_rows(min_row=2):
+    for cell in row:
+        print(cell.value, end='\t')
+        if cell.value == "Maria":
+            worksheet.cell(cell.row, 2, 23)
+    print()
+
+worksheet['B3'].value = 14
+workbook.save(WORKBOOK_PATH)
+
+
 
